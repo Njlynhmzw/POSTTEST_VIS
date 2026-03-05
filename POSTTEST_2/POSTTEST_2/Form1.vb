474c70bf845fb11e676f6cd112aa86e84976c1d6
Njlynhmzw <julyanahnou@gmail.com>
2026-03-05 21:50:25 +0800

    pt2 direct

---
+++ POSTTEST_2/POSTTEST_2/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub btntambah_Click(sender As Object, e As EventArgs) Handles btntambah.Click
+
+        TambahBuku(txtjudul.Text, txtgenre.Text)
+
+        ListBox1.Items.Clear()
+        ListBox1.Items.AddRange(TampilBuku.Split(vbCrLf))
+
+        txtjudul.Clear()
+        txtgenre.Clear()
+
+    End Sub
+
+
+    Private Sub btnhapus_Click(sender As Object, e As EventArgs) Handles btnhapus.Click
+
+        If CekBuku(txtjudulbuku.Text) = False Then
+
+            MessageBox.Show("Judul buku tidak ada dalam daftar ", "Error")
+
+        Else
+
+            HapusBuku(txtjudulbuku.Text)
+
+            ListBox1.Items.Clear()
+            ListBox1.Items.AddRange(TampilBuku.Split(vbCrLf))
+
+        End If
+
+        txtjudulbuku.Clear()
+
+    End Sub
+
+End Class
